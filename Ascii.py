3e02c518c6e44a81aee3f2f409baf002d1bf01d0
G Sri Gayathri <srigayathrig4@gmail.com>
2026-02-09 22:53:02 +0530

    Add files via upload

---
+++ Ascii.py
@@ -0,0 +1,9 @@
+char_a = 'A'
+char_b = 'b'
+ascii_value_a = ord(char_a)
+ascii_value_b = ord(char_b)
+print("ascii value of A : ", ascii_value_a)
+print("ascii value of b : ", ascii_value_b)
+ascii_value = 68
+character = chr(ascii_value)
+print("character : ", character)
